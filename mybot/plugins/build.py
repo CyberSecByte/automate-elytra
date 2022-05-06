3125d0aa72539d2ee5ceb0721b7ce8c9537991d2
CyberSecByte <65858180+CyberSecByte@users.noreply.github.com>
2022-05-06 18:40:59 +0530

    jenkins

--- mybot/plugins/build.py
+++ mybot/plugins/build.py
@@ -9,24 +9,17 @@ async def build(event):
     all = merainput.split("/build")[1].split(" ")
     valueone = all[1]
     valuetwo = all[2]
-    m = await event.reply("```Launching Env Command...```")
-    os.system(f"cd ../elytra && bash build/envsetup.sh > pasteenv.txt")
+    f = await event.reply("```Setting up the variables...```")
+    os.system(f"echo -e DEVICE={valueone}\nBUILD_TYPE={valuetwo} > inputvars.conf")
     i = open("../elytra/pasteenv.txt", "r")
+    varslink = await paste(i.read())
+    final_varslink = f"Output For Environment is Pasted [Here ]({varslink})"
+    await m.edit(final_varslink)
+    m = await event.reply("```Launching Env Command...```")
+    os.system(f"bash jenkin.sh > jenkins.txt")
+    i = open("jenkins.txt", "r")
     envlink = await paste(i.read())
-    final_envlink = f"Output For Environment is Pasted [Here ]({envlink})"
+    final_envlink = f"Build Process is [Here ]({envlink})"
     await m.edit(final_envlink)
-    os.remove("../elytra/pasteenv.txt")
-    n = await event.reply("```Launching Lunch command...```")
-    os.system(f"cd ../elytra && lunch elytra_{valueone}-{valuetwo} > pastelunch.txt")
-    i = open("../elytra/pastelunch.txt", "r")
-    lunchlink = await paste(i.read())
-    final_lunchlink = f"Output For Lunch Command Is Pasted [Here]({lunchlink})"
-    await n.edit(final_lunchlink)
-    os.remove("../elytra/pastelunch.txt")
-    o = await event.reply("```Launching Brunch Command```")
-    os.system(f"cd ../elytra && brunch elytra_{valueone}-{valuetwo} > pastebrunch.txt")
-    i = open("../elytra/pastebrunch.txt", "r")
-    brunchlink = await paste(i.read())
-    final_brunchlink = f"Output For brunch Command Is Pasted [Here]({brunchlink})"
-    await o.edit(final_brunchlink)
-    os.remove("../elytra/pastebrunch.txt")
+    os.remove("jenkins.txt")
+   
