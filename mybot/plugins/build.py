acb4eed679aa08fa8bb01473eb51767a96d9e753
Cybersecbyte <cybersecbyte@gmail.com>
2022-05-16 14:03:25 +0530

    push

--- mybot/plugins/build.py
+++ mybot/plugins/build.py
@@ -10,23 +10,20 @@ async def build(event):
     valueone = all[1]
     valuetwo = all[2]
     m = await event.reply("```Launching Env Command...```")
-    os.system(f"cd ../elytra && bash build/envsetup.sh > pasteenv.txt")
-    i = open("../elytra/pasteenv.txt", "r")
+    os.system(f"echo bash envsetup.sh > ../elytra/build/pasteenv.sh")
+    i = open("../elytra/build/pasteenv.sh", "r")
     envlink = await paste(i.read())
     final_envlink = f"Output For Environment is Pasted [Here ]({envlink})"
     await m.edit(final_envlink)
-    os.remove("../elytra/pasteenv.txt")
-    n = await event.reply("```Launching Lunch command...```")
-    os.system(f"bash launchlunch.sh {valueone} {valuetwo} > pastelunch.txt")
-    i = open("pastelunch.txt", "r")
+    n = await event.reply("```Saving Lunch params...```")
+    os.system(f"echo launchlunch.sh {valueone} {valuetwo} > ../elytra/build/pastelunch.sh")
+    i = open("../elytra/build/pastelunch.sh", "r")
     lunchlink = await paste(i.read())
     final_lunchlink = f"Output For Lunch Command Is Pasted [Here]({lunchlink})"
     await n.edit(final_lunchlink)
-    os.remove("pastelunch.txt")
     o = await event.reply("```Launching Brunch Command```")
-    os.system(f"bash launchbrunch.sh {valueone} {valuetwo} > pastebrunch.txt")
-    i = open("pastebrunch.txt", "r")
+    os.system(f"echo launchbrunch.sh {valueone} {valuetwo} > ../elytra/build/pastebrunch.sh")
+    i = open("../elytra/build/pastebrunch.txt", "r")
     brunchlink = await paste(i.read())
     final_brunchlink = f"Output For brunch Command Is Pasted [Here]({brunchlink})"
     await o.edit(final_brunchlink)
-    os.remove("pastebrunch.txt")
