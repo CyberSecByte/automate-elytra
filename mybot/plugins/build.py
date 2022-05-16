cd3488757bec2d8d85d1047e3e61c6def995d142
CyberSecByte <65858180+CyberSecByte@users.noreply.github.com>
2022-05-16 14:41:17 +0530

    push

--- mybot/plugins/build.py
+++ mybot/plugins/build.py
@@ -16,13 +16,13 @@ async def build(event):
     final_envlink = f"Output For Environment is Pasted [Here ]({envlink})"
     await m.edit(final_envlink)
     n = await event.reply("```Saving Lunch params...```")
-    os.system(f"echo lunch_{valueone}-{valuetwo} > ../elytra/build/pastelunch.sh")
+    os.system(f"echo lunch elytra_{valueone}-{valuetwo} > ../elytra/build/pastelunch.sh")
     i = open("../elytra/build/pastelunch.sh", "r")
     lunchlink = await paste(i.read())
     final_lunchlink = f"Output For Lunch Command Is Pasted [Here]({lunchlink})"
     await n.edit(final_lunchlink)
     o = await event.reply("```Launching Brunch Command```")
-    os.system(f"echo brunch_{valueone}-{valuetwo} > ../elytra/build/pastebrunch.sh")
+    os.system(f"echo brunch elytra_{valueone}-{valuetwo} > ../elytra/build/pastebrunch.sh")
     i = open("../elytra/build/pastebrunch.sh", "r")
     brunchlink = await paste(i.read())
     final_brunchlink = f"Output For brunch Command Is Pasted [Here]({brunchlink})"
